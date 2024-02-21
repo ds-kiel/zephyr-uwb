29ed39ea36a35ea2a80ec5d055d8313c717eb938
Patrick Rathje <git@patrickrathje.de>
2024-02-21 13:54:12 +0100

    reenable get_node_number

--- src/main.c
+++ src/main.c
@@ -370,7 +370,7 @@ int main(void) {
 
 
     LOG_INF("Getting node id");
-    int16_t signed_node_id = 0; //get_node_number(get_own_node_id());
+    int16_t signed_node_id = get_node_number(get_own_node_id());
 
     if (signed_node_id < 0) {
         LOG_INF("Node number NOT FOUND! Shutting down :( I am: 0x%04hx", get_own_node_id());
